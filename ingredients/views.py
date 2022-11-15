319745be853835187af3edbed93858b2925d06b0
eun10 <dnsrmd99@gmail.com>
2022-11-15 15:40:41 +0900

    여러 재료들이 모두 사용되는 레시피 추천

--- ingredients/views.py
+++ ingredients/views.py
@@ -9,7 +9,6 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 import logging
-import cv2
 import tensorflow as tf
 from django.db import models
 from django import forms 
@@ -18,32 +17,18 @@ from .models import Post, RecipeList
 from .forms import TextForm
 from PIL import Image
 from django.views.decorators.csrf import csrf_exempt
+from django.core.paginator import Paginator
+import logging
+from django.shortcuts import render
+from django.db import connection
+from django.db.models.query_utils import Q
+import operator
+from functools import reduce
 
 
-
-# def StockListView(request):
-#     try:
-#         cursor = connection.cursor()
-#         query = '''INSERT INTO ingredients_post (ingredient)
-#                     SELECT result
-#                     FROM ingredients_mnistimage'''
-#         result = cursor.execute(query)
-#         stocks = cursor.fetchall()
-
-#         connection.commit()
-#         connection.close()
-
-#     except:
-#         connection.rollback()
-#         print("Failed Selecting in StockList")
-    
-#     context = {'stocks' : stocks}
-
-#     return render(request, 'post_list.html', context)
-
+###### 나의 냉장고 목록
 def post_list(request):
     posts = Post.objects.all().order_by('-pk') # 데이터베이스에 쿼리를 날려 원하는 레코드 가져오기
-
 
     return render(
         request,
@@ -54,27 +39,24 @@ def post_list(request):
     )
 
 
-import logging
-
+###### 로그 함수
 # 로그 생성
 logger = logging.getLogger()
-
 # 로그의 출력 기준 설정
 logger.setLevel(logging.INFO)
-
 # log 출력 형식
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 # log 출력
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
-
 # log를 파일에 출력
 file_handler = logging.FileHandler('my.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
+
+###### 선택 삭제
 @csrf_exempt
 def delete_all(request):
     logger.info('================ delete_all')
@@ -90,7 +72,9 @@ def delete_all(request):
             logger.info('deleted.....')
         
         return redirect('ingredients')
-    
+
+
+###### 선택 레시피 추천    
 @csrf_exempt
 def recommend(request):
     logger.info('================ recommend')
@@ -98,23 +82,31 @@ def recommend(request):
     if(request.method == 'POST'):
         ids = request.POST["rec_ids"]
         ids = ids.split(',')
+        keywords = ''
+        
+        q_list = []
+        
         for idx in range(len(ids)):
             id = ids[idx]
             ingredient = Post.objects.get(id=id)
             keyword = ingredient.ingredient
+            keywords += ' ' + keyword
             
-            recipe_list = RecipeList.objects.all().order_by('-rc_rec')
-            recipe_list = recipe_list.filter(rc_ing__icontains=keyword)
-
-            # ingredient.objects.values 
-            
-            logger.info('recommending.....')
+            query = Q(rc_ing__icontains=keyword)
+            q_list.append(query)
+                
+        
+        logger.info(q_list)
+        recipe_list = RecipeList.objects.all()
+        recipe_list = recipe_list.filter(reduce(operator.and_, q_list)).order_by('-rc_rec')
+        print(recipe_list[0].rc_name)   
         
     return render(
             request,
-            'ingredients/recipe_list.html',{'recipe_list':recipe_list, 'keywords': keyword})
+            'ingredients/recipe_list.html',{'recipe_list':recipe_list,'keywords': keywords})
     
-          
+    
+###### 텍스트로 입력          
 def upload_text(request):
     if(request.method == 'POST'):
         form=TextForm(request.POST)
@@ -130,28 +122,7 @@ def upload_text(request):
             'ingredients/upload_text.html',context)
 
 
-def upload_text(request):
-    if(request.method == 'POST'):
-        form=TextForm(request.POST)
-        if form.is_valid():
-            ing = form.save(commit=False)
-            ing.save()
-            return redirect('ingredients')
-    else:
-        form=TextForm()
-    context={'form':form}
-    return render(
-            request,
-            'ingredients/upload_text.html',context)
-
-
-# def upload_image(request):
-    
-#     return render(
-#         request,
-#         'ingredients/upload_image.html',
-#     )
-
+###### 이미지로 입력(모델 적용)
 class MnistImageCreate(CreateView):
     model= MnistImage
     fields=['head_image']
@@ -166,17 +137,10 @@ def image_result(request,pk):
     logging.error('Something unexpected and important happened.')
     logging.critical('OMG!!! A critical error happend and the code cannot run!')
     
-    # img = Image.open(data.head_image)
-    # img = np.array(img)
-    # img = cv2.resize(img,dsize=(128,128),interpolation=cv2.INTER_LINEAR)
-    # img_batch = np.expand_dims(img, 0)
-    
     img = np.array(Image.open(data.head_image).resize((128, 128)))
     img_list = []
     img_list.append(np.array(img))
     x = np.asarray(img_list)
-    # pr_mask = model.predict(x).round()
-    
     
     loaded_model = load_model("model_lb5_128_plus_vgg_8282.h5")
     pred = loaded_model.predict(x)
@@ -186,7 +150,6 @@ def image_result(request,pk):
     result = label_index[pred]
     
     data.result = result
-    # logging.debug(cust)
     data.save()
     
     try:
@@ -207,8 +170,6 @@ def image_result(request,pk):
         connection.rollback()
         print("Failed Selecting in StockList")
     
-    # context = {'stocks' : stocks}
-    
     return render(
         request,
         'ingredients/result.html',
@@ -218,43 +179,10 @@ def image_result(request,pk):
     )
     
 
-from django.shortcuts import render
-from django.db import connection
-
-# def recipe_list(request):
-#     posts = Post.objects.all().order_by('-pk') # 데이터베이스에 쿼리를 날려 원하는 레코드 가져오기
-#     Blog.objects.filter(pk__in=[1, 4, 7])
-#     return render(
-#         request,
-#         'ingredients/post_list.html',
-#         {
-#             'posts':posts,
-#         }
-#     )
-
-# def recipe_list(request):
-#     candidates = 10000recipe.objects.all()
-#     context = {'candidates':candidates}
-#         #context에 모든 어린이 정보를 저장
-#     return render(request, 'elections/index.html', context)
-#         #context안에 있는 어린이 정보를 index.html로 전달
-
-# class RecipeListView(ListView):
-#     model= RecipeList
-#     fields=['rc_num', 'rc_name','rc_diff','rc_time','rc_ing']
-    
+###### 레시피 페이지    
 def recipe_list(request):
     recipelists = RecipeList.objects.all()
-    # recipelists = RecipeList.objects.filter(rc_ing__in=['당근'])
-    # lists = ['당근','대파']
 
-    # for inglists in lists:
-    #     recipelists = RecipeList.objects.filter(rc_ing__in=inglists)
-    # rc_ing = ['당근','대파']
-    # for tag in rc_ing:
-    #     ids = list(RecipeList.objects.filter(rc_ing__contains=tag).values_list("rc_ing", flat=True))
-    #     recipelists = RecipeList.objects.filter(rc_ing__contains=ids)
-        # recipelists = RecipeList.objects.filter(rc_ing__in='당근') 
 
     return render(
         request,
